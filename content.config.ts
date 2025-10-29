de2c1b3d3758c777d4dfac19fc27f3df228159ae
Radu Stanciu <radu@kangu.ro>
2025-10-29 19:33:19 +0200

    removed @nuxt/config depdency, added minimark

--- content.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineContentConfig, defineCollection} from '@nuxt/content'
-
-export default defineContentConfig({
-    collections: {
-        // this is not really used, just to silence the config warning
-        content: defineCollection({type: 'page'})
-    }
-})
